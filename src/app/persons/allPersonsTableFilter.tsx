b12a2823ca9a8f5457ab5e6974d17d9e8ca7cb23
Santeri Korpela <sjkorpela03@gmail.com>
2026-05-19 16:06:41 +0300

    Remove year filter checkboxes

--- src/app/persons/allPersonsTableFilter.tsx
+++ src/app/persons/allPersonsTableFilter.tsx
@@ -17,27 +17,24 @@ export default function AllPersonsTableFilter({filter, setFilter}: Props) {
     const [gender, setGender] = useState<Gender | null>(filter?.gender ?? null)
 
     const [bornAfter, setBornAfter] = useState<number | null>(filter?.bornAfter ?? null)
-    const [bornAfterActive, setBornAfterActive] = useState<boolean | null>(false)
 
     const [bornBefore, setBornBefore] = useState<number | null>(filter?.bornBefore ?? null)
-    const [bornBeforeActive, setBornBeforeActive] = useState<boolean | null>(false)
 
     const [diedAfter, setDiedAfter] = useState<number | null>(filter?.diedAfter ?? null)
-    const [diedAfterActive, setDiedAfterActive] = useState<boolean | null>(false)
 
     const [diedBefore, setDiedBefore] = useState<number | null>(filter?.diedBefore ?? null)
-    const [diedBeforeActive, setDiedBeforeActive] = useState<boolean | null>(false)
 
     useEffect(() => {
+        console.log(bornAfter)
         setFilter({
             deceased: deceased,
             gender: gender,
-            bornAfter: bornAfterActive ? bornAfter : null,
-            bornBefore: bornBeforeActive ? bornBefore : null,
-            diedAfter: diedAfterActive ? diedAfter : null,
-            diedBefore: diedBeforeActive ? diedBefore : null,
+            bornAfter: bornAfter,
+            bornBefore: bornBefore,
+            diedAfter: diedAfter,
+            diedBefore: diedBefore
         })
-    }, [setFilter, deceased, gender, bornBefore, diedAfter, diedBefore, bornAfterActive, bornAfter, bornBeforeActive, diedAfterActive, diedBeforeActive]);
+    }, [setFilter, deceased, gender, bornBefore, diedAfter, diedBefore, bornAfter]);
 
     function parseDeceased(value: string) {
         if (value == "true") return true;
@@ -49,13 +46,9 @@ export default function AllPersonsTableFilter({filter, setFilter}: Props) {
         setDeceased(null);
         setGender(null);
         setBornAfter(null);
-        setBornAfterActive(false);
         setBornBefore(null);
-        setBornBeforeActive(false);
         setDiedAfter(null);
-        setDiedAfterActive(false);
         setDiedBefore(null);
-        setDiedBeforeActive(false);
 
         filterForm.current?.reset()
     }
@@ -101,28 +94,24 @@ export default function AllPersonsTableFilter({filter, setFilter}: Props) {
                         <td>Syntynyt jälkeen:</td>
                         <td>
                             <FormYearInput year={bornAfter} setYear={setBornAfter} testingId={"bornAfter"} />
-                            <FormCheckbox state={bornAfterActive} setState={setBornAfterActive} testingId={"bornAfterActive"} />
                         </td>
                     </tr>
                     <tr>
                         <td>Syntynyt ennen:</td>
                         <td>
                             <FormYearInput year={bornBefore} setYear={setBornBefore} testingId={"bornBefore"} />
-                            <FormCheckbox state={bornBeforeActive} setState={setBornBeforeActive} testingId={"bornBeforeActive"} />
                         </td>
                     </tr>
                     <tr>
                         <td>Kuollut jälkeen:</td>
                         <td>
                             <FormYearInput year={diedAfter} setYear={setDiedAfter} testingId={"diedAfter"} />
-                            <FormCheckbox state={diedAfterActive} setState={setDiedAfterActive} testingId={"diedAfterActive"} />
                         </td>
                     </tr>
                     <tr>
                         <td>Kuollut ennen:</td>
                         <td>
                             <FormYearInput year={diedBefore} setYear={setDiedBefore} testingId={"diedBefore"} />
-                            <FormCheckbox state={diedBeforeActive} setState={setDiedBeforeActive} testingId={"diedBeforeActive"} />
                         </td>
                     </tr>
                     <tr>
